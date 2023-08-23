fe4e3cffc2b084337d0f6fd252ebb5ee064efff9
kurtyoon <akakertcobain@naver.com>
2023-08-23 11:36:33 +0900

    Feat: 프로젝트 초기화 및 DB 세팅

--- src/main/java/com/FarmSystem/TEATime/TEATimeApplication.java
+++ src/main/java/com/FarmSystem/TEATime/TEATimeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TEATimeApplication {
+public class TeaTimeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TEATimeApplication.class, args);
+		SpringApplication.run(TeaTimeApplication.class, args);
 	}
 
 }
--- src/main/java/com/FarmSystem/TEATime/jwt/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.FarmSystem.TEATime.jwt.repository;
-
-import com.FarmSystem.TEATime.jwt.domain.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
