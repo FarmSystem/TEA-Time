df1d61c0d206147a72f731ef3f79f6b194d9dc68
leechangjoon <2018110021@dongguk.edu>
2023-09-07 12:54:50 +0900

    Refactor: Tab 경로 변경

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import {TabNavigation} from './navigations/Tab';
-import {NavigationContainer} from '@react-navigation/native';
+import React from "react";
+// import {TabNavigation} from './navigations/Tab';
+import { TabNavigation } from "./src/navigations/Tab";
+import { NavigationContainer } from "@react-navigation/native";
 
 const App = () => {
-    return (
-        <NavigationContainer>  
-          <TabNavigation />
-        </NavigationContainer>
-
-    );
-  };
+  return (
+    <NavigationContainer>
+      <TabNavigation />
+    </NavigationContainer>
+  );
+};
 
 /*const App = () => {
     return (
