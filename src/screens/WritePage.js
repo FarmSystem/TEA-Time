578462418059c1f5ae0655aee0d0589584b92e3c
ryann1203 <oxford1203@naver.com>
2023-09-21 16:33:13 +0900

    writebox 일기 작성란

--- src/screens/WritePage.js
+++ src/screens/WritePage.js
@@ -40,15 +40,39 @@ justify-content: space-around;
 background-color : ${({theme}) => theme.itemBackground};
 border : 2px solid #D9D9D9;
 color : black;
-font-size : 22px;
+font-size : 20px;
 font-weight : bold;
 text-align : center;
+`;
+
+const WriteBox = styled.TextInput`
+width : 350px;
+margin : 3px 0;
+padding : 15px 20px;
+border-radius : 5px;
+justify-content: space-around;
+background-color : ${({theme}) => theme.itemBackground};
+border : 2px solid #D9D9D9;
+color : black;
+font-size : 17px;
+`;
+
+const ImageBox = styled.TouchableOpacity`
+width : 350px;
+margin : 3px 0;
+padding : 15px 20px;
+border-radius : 5px;
+justify-content: space-around;
+background-color : ${({theme}) => theme.itemBackground};
+border : 2px solid #D9D9D9;
+color : black;
+font-size : 16px;
 `;
 
 const styles = StyleSheet.create({
   Save: {
     position: "absolute",
-    top: -315,
+    top: -120,
     left: 220,
   },
 });
@@ -63,9 +87,14 @@ const WritePage = () => {
             <Title>저장하기</Title>
           </Savebutton>
         </View>
-        <StyledInput style = {{top : -210}}>
+        <StyledInput style = {{position : "absolute", top : -30}}>
           <Text placeholder = "yyyy/mm/dd" placeholderTextColor = "#3498db"/>
         </StyledInput>
+        <WriteBox style = {{top : 48, height : 60, fontWeight : 'bold'}}/>
+        <WriteBox style = {{top : 40, height : 250, fontSize : 15}}/>
+        <ImageBox style = {{top : 55, height : 160}}>
+          <Title style = {{color : 'black', fontWeight : 'bold'}}>사진 첨부하기</Title>
+        </ImageBox>
        </ThemeProvider>
       </View>
     </Container>
