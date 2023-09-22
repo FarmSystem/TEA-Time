4c9c0688d1d93087fff2690bf74359359b8d3d2a
ryann1203 <oxford1203@naver.com>
2023-09-22 21:42:06 +0900

    Feat : writepage 사진 업로드 및 이모티콘 추가

--- src/screens/WritePage.js
+++ src/screens/WritePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, StyleSheet, View, TouchableOpacity } from 'react-native';
 import styled, {ThemeProvider} from "styled-components/native";
 import { theme } from "../theme";
+import * as ImagePicker from "expo-image-picker";
 
 
 const Container = styled.View`
@@ -12,9 +13,9 @@ const Container = styled.View`
 `;
 
 const Savebutton = styled.TouchableOpacity`
-background-color : #677a46;
+background-color : #799462;
 border-radius : 20px;
-width :120px;
+width :110px;
 padding: 12px;
 margin-vertical: 40px;
 height : 38px;
@@ -46,10 +47,10 @@ text-align : center;
 `;
 
 const WriteBox = styled.TextInput`
-width : 350px;
+width : 280px;
 margin : 3px 0;
 padding : 15px 20px;
-border-radius : 5px;
+border-radius : 3px;
 justify-content: space-around;
 background-color : ${({theme}) => theme.itemBackground};
 border : 2px solid #D9D9D9;
@@ -57,16 +58,50 @@ color : black;
 font-size : 17px;
 `;
 
-const ImageBox = styled.TouchableOpacity`
+const ImageBox = styled.View`
 width : 350px;
 margin : 3px 0;
 padding : 15px 20px;
-border-radius : 5px;
+border-radius : 3px;
 justify-content: space-around;
 background-color : ${({theme}) => theme.itemBackground};
 border : 2px solid #D9D9D9;
 color : black;
 font-size : 16px;
+`;
+
+const PutImage = styled.TouchableOpacity`
+background-color :#cbe3b3;
+border-radius : 10px;
+width :130px;
+padding: 8px;
+margin-vertical: 40px;
+height : 40px;
+justify-content : space-around;
+font-size : 14px;
+`;
+
+const Emoji = styled.TouchableOpacity`
+width : 73px;
+height : 60px;
+margin : 3px 0;
+padding : 15px 20px;
+border-radius : 3px;
+justify-content: space-around;
+background-color : ${({theme}) => theme.itemBackground};
+border : 2px solid #D9D9D9;
+color : black;
+`;
+
+const EmojiContainer = styled.View`
+background-color : #a9d1b1;
+border-radius : 15px;
+width :70px;
+padding: 12px;
+margin-vertical: 40px;
+height : 50px;
+justify-content : space-around;
+font-size : 20px;
 `;
 
 const styles = StyleSheet.create({
@@ -76,6 +111,26 @@ const styles = StyleSheet.create({
     left: 220,
   },
 });
+
+const pickImage = async() => {
+  console.log('이미지 선택');
+  let imageData = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.All,
+    allowsEditing : true,
+    aspect : [4, 4],
+    quality : 1,
+  });
+  console.log('이미지 데이터: ', imageData);
+};
+
+
+//이모티콘 고르는 창...
+const pickEmoji = () => {
+  return (
+    <EmojiContainer>
+    </EmojiContainer>
+  )
+}
 
 const WritePage = () => {
   return (
@@ -87,14 +142,23 @@ const WritePage = () => {
             <Title>저장하기</Title>
           </Savebutton>
         </View>
-        <StyledInput style = {{position : "absolute", top : -30}}>
-          <Text placeholder = "yyyy/mm/dd" placeholderTextColor = "#3498db"/>
-        </StyledInput>
-        <WriteBox style = {{top : 48, height : 60, fontWeight : 'bold'}}/>
-        <WriteBox style = {{top : 40, height : 250, fontSize : 15}}/>
+
+        <StyledInput style = {{position : "absolute", top : -30}}/>
+        <View style = {{flexDirection : 'row'}}>
+          <WriteBox style = {{top : 48, height : 60, fontWeight : 'bold'}}/>
+          <Emoji style = {{position : "absolute", left : 277, top : 48}}
+           onPress = {pickEmoji}> 
+           <Text>😀</Text>
+          </Emoji>
+        </View>
+        <WriteBox style = {{top : 40, height : 250, width : 350, fontSize : 15}}/>
         <ImageBox style = {{top : 55, height : 160}}>
-          <Title style = {{color : 'black', fontWeight : 'bold'}}>사진 첨부하기</Title>
+          <PutImage style = {{left : 85}}>
+            <Title style = {{color : 'black', fontWeight : 'bold'}}
+             onPress={pickImage}>사진 첨부하기</Title>
+          </PutImage>
         </ImageBox>
+
        </ThemeProvider>
       </View>
     </Container>
