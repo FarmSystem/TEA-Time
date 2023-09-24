654a8374a2da0fd4c49dbf17cc74ded0e9627153
Jang99u <alswkdrb@dgu.ac.kr>
2023-09-24 18:30:22 +0900

    Feat: 모델 테스트파일 추가

--- BERTmodel/model_test.py
+++ BERTmodel/model_test.py
@@ -2,4 +2,11 @@ from emotion_model import EmotionAnalysis
 
 Analysis = EmotionAnalysis()
     
-Analysis.print_emotion(["오늘 하루 기분이 정말 좋았어", "왜냐면 회사에서 인정받았거든"])
+with open("BERTmodel/model_test.txt") as text :
+    text_test = text.readlines()
+    text_test = "".join(text_test)
+    
+Analysis.analyze_emotion(text_test)
+
+# 패딩 길이 조절 필요
+# ValueError: 128 is not a valid PaddingStrategy, please select one of ['longest', 'max_length', 'do_not_pad']
