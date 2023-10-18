50a28712a5b44f4cba3c247d96e21251182309e9
kurtyoon <akakertcobain@naver.com>
2023-10-18 12:43:38 +0900

    Feat: FastAPI config 지정 및 router 세팅

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,12 @@
-FROM python:3.7.4-slim-buster
+FROM python:3.11-alpine
+
+WORKDIR /app
+
+RUN /usr/local/bin/python -m pip install --upgrade pip
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+import uvicorn
+from dotenv import load_dotenv
+from app.api.api import api_router
+from app.core.config import settings
+
+load_dotenv()
+
+"""
+OpenAPI docs: http://localhost:8080/api/docs
+"""
+app = FastAPI(
+    title=settings.PROJECT_NAME, docs_url="/api/docs", openapi_url="/api"
+)
+
+
+"""
+CORS middleware 지정
+"""
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+
+"""
+API 라우터 경로를 /api/v1으로 지정
+"""
+app.include_router(api_router, prefix=settings.API_V1_STR)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8080)
