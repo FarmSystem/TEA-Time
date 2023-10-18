2da01fcbe8ae9a32fd23c7eb05d94b0136a23ca8
kurtyoon <akakertcobain@naver.com>
2023-10-18 17:24:52 +0900

    Feat: 자연어 처리 모델 추가

--- app/api/api.py
+++ app/api/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import core
+from app.api.endpoints import predict
 
 api_router = APIRouter()
 api_router.include_router(core.router, tags=["core"])
+api_router.include_router(predict.router, tags=["predict"])
