0b00a3fd013733f2c489924b2627c8554a277e86
kurtyoon <akakertcobain@naver.com>
2023-11-03 13:21:24 +0900

    Feat: Swagger 관련 Configuration 설정

---
+++ api/src/main/java/com/farm/api/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.farm.api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-api")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Tea Time API")
+                        .version("1.0")
+                        .description("Tea Time API v1.0")
+                );
+    }
+}
