fd9c8d70da27ee954ca070ae43a568e0443066fa
kurtyoon <akakertcobain@naver.com>
2023-11-03 13:21:12 +0900

    Feat: Cors 관련 Configuration 설정

---
+++ api/src/main/java/com/farm/api/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.farm.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        /**
+         * 도메인 정하면 변경
+         */
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080", "http://localhost:8000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.addAllowedHeader("*");
+        configuration.addExposedHeader("authorization");
+        configuration.addExposedHeader("reauthorization");
+
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter(source);
+    }
+}
