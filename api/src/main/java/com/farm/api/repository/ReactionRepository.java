571ff49c3729a91e230f93902f3b51797b9aeb53
kurtyoon <akakertcobain@naver.com>
2023-11-10 13:55:13 +0900

    Feat: 각 Domain 별 Repository 생성 및 User natureQuery 작성

---
+++ api/src/main/java/com/farm/api/repository/ReactionRepository.java
@@ -0,0 +1,9 @@
+package com.farm.api.repository;
+
+import com.farm.api.domain.Reaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReactionRepository extends JpaRepository<Reaction, Long> {
+}
