122be75f4e32bd51a03cd646ea068ba3c3dd2042
leechangjoon <2018110021@dongguk.edu>
2024-01-03 20:24:44 +0900

    [Feat] 설정 탭 레이아웃 구현 + 클릭시 페이지 라우팅 임의 구현

--- lib/screen/setting/setting_screen.dart
+++ lib/screen/setting/setting_screen.dart
@@ -3,19 +3,65 @@ import 'package:flutter/material.dart';
 import '../base/default_appbar.dart';
 
 class SettingScreen extends StatelessWidget {
-  const SettingScreen({super.key});
+  const SettingScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(kToolbarHeight),
         child: DefaultAppBar(
           title: 'Setting',
         ),
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          buildSettingItem(context, '달력', 'calendar'),
+          buildSettingItem(context, '마이 프로필', 'my_profile'),
+          buildSettingItem(context, '내 정보 설정', 'account_settings'),
+          buildSettingItem(context, '레벨 보기', 'view_level'),
+        ],
+      ),
+    );
+  }
+
+  Widget buildSettingItem(BuildContext context, String title, String route) {
+    return InkWell(
+      onTap: () {
+        // 각 항목을 누를 때 해당 페이지로 이동
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SettingDetailScreen(route)),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(title),
+            Icon(Icons.arrow_forward_ios),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingDetailScreen extends StatelessWidget {
+  final String route;
+
+  SettingDetailScreen(this.route);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(route), // 실제 페이지로 변경 가능
+      ),
       body: Center(
-        child: Text('Setting Screen'),
+        child: Text('This is the $route page.'),
       ),
     );
   }
