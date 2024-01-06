7aa481a8880c5adeb9afa0488197a70b76b32bd8
leechangjoon <2018110021@dongguk.edu>
2024-01-06 21:55:24 +0900

    [Docs] table_calendar 패키지 추가 선언

--- lib/screen/diary/notification_page.dart
+++ lib/screen/diary/notification_page.dart
@@ -16,19 +16,15 @@ class NotificationPage extends StatelessWidget {
         title: const Text('알림'),
       ),
       body: ListView.builder(
-        itemCount: 5, // 알림의 갯수, 예시로 5개를 설정했습니다.
+        itemCount: 5, // 알림의 갯수, 예시로 5개를 설정
         itemBuilder: (BuildContext context, int index) {
           return ListTile(
             leading: CircleAvatar(
               backgroundColor: Colors.grey,
-              // 알림 아이콘 혹은 사용자의 이미지가 들어갈 수 있습니다.
             ),
-            title: Text('네이버의 최신회원 데이터에 날짜를 남겼어요.'),
+            title: Text('XXX님이 회원님의 다이어리에 반응을 남겼어요.'),
             subtitle: Text('202X.XX.XX.'),
-            // 여기에 날짜 형식에 맞는 실제 데이터를 넣어야 합니다.
-            onTap: () {
-              // 알림을 탭했을 때의 동작을 여기에 구현합니다.
-            },
+            onTap: () {},
           );
         },
       ),
