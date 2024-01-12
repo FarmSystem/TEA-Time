cffaf9bf59348a5216351b1fe0cba3c9ef81faaf
kurtyoon <akakertcobain@naver.com>
2024-01-13 00:40:53 +0900

    [Feat] 추천친구 로직에 대한 뷰모델을 생성한다.

---
+++ lib/viewModel/community/recommend_view_model.dart
@@ -0,0 +1,37 @@
+import 'package:get/get.dart';
+
+class RecommendViewModel extends GetxController {
+  late final RxString _username;
+  late final RxString _profileImage;
+  late final RxInt _diaryCount;
+
+  RxString get username => _username;
+  RxString get profileImage => _profileImage;
+  RxInt get diaryCount => _diaryCount;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    _username = "".obs;
+    _profileImage = "".obs;
+    _diaryCount = 0.obs;
+  }
+
+  void setUsername(String username) {
+    _username.value = username;
+  }
+
+  void setProfileImage(String profileImage) {
+    _profileImage.value = profileImage;
+  }
+
+  void setDiaryCount(int diaryCount) {
+    _diaryCount.value = diaryCount;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
