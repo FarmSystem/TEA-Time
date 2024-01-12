39d39d669715bc2f5d483a00fb607443a55c36c0
kurtyoon <akakertcobain@naver.com>
2024-01-13 00:39:02 +0900

    [Feat] API에 해당하는 프로바이더를 생성한다.

---
+++ lib/provider/analysis/analysis_provider.dart
@@ -0,0 +1,54 @@
+import 'package:dio/dio.dart';
+import 'package:tea_time/provider/base/http_util.dart';
+
+class AnalysisProvider {
+  static final Dio authDio = HttpUtil().authDio;
+
+  Future getMonthlyAnalysis() async {
+    try {
+      final response = await authDio.get("/analysis/monthly");
+
+      if (response.statusCode == 200) {
+        return response.data["data"];
+      } else {
+        return throw Exception(
+          "API 요청이 실패했습니다. - ${response.data["error"]["code"]} ${response.data["error"]["message"]}"
+        );
+      }
+    } on Exception catch(e) {
+      return Future.error(e);
+    }
+  }
+
+  Future getWeeklyAnalysis() async {
+    try {
+      final response = await authDio.get("/analysis/weekly");
+
+      if (response.statusCode == 200) {
+        return response.data["data"];
+      } else {
+        return throw Exception(
+          "API 요청이 실패했습니다. - ${response.data["error"]["code"]} ${response.data["error"]["message"]}"
+        );
+      }
+    } on Exception catch(e) {
+      return Future.error(e);
+    }
+  }
+
+  Future getMemberAnalysis(int diaryId) async {
+    try {
+      final response = await authDio.get("/analysis/$diaryId");
+
+      if (response.statusCode == 200) {
+        return response.data["data"];
+      } else {
+        return throw Exception(
+          "API 요청이 실패했습니다. - ${response.data["error"]["code"]} ${response.data["error"]["message"]}"
+        );
+      }
+    } on Exception catch(e) {
+      return Future.error(e);
+    }
+  }
+}
---
+++ lib/provider/community/community_provider.dart
@@ -0,0 +1,88 @@
+import 'package:dio/dio.dart';
+import 'package:tea_time/provider/base/http_util.dart';
+
+class CommunityProvider {
+  static final Dio authDio = HttpUtil().authDio;
+
+  Future getCommunity(int page, int size) async {
+    try {
+      final response = await authDio.get("/community?page=$page&size=$size");
+
+      if (response.statusCode == 200) {
+        return response.data["data"];
+      } else {
+        return throw Exception(
+            "API 요청이 실패했습니다. - ${response.data["error"]["code"]} ${response.data["error"]["message"]}"
+        );
+      }
+    } on Exception catch(e) {
+      return Future.error(e);
+    }
+  }
+
+  Future getMember() async {
+    try {
+      final response = await authDio.get("/community/member");
+
+      if (response.statusCode == 200) {
+        return response.data["data"];
+      } else {
+        return throw Exception(
+            "API 요청이 실패했습니다. - ${response.data["error"]["code"]} ${response.data["error"]["message"]}"
+        );
+      }
+    } on Exception catch(e) {
+      return Future.error(e);
+    }
+  }
+
+  Future memberFollow(int memberId) async {
+    try {
+      final response = await authDio.post("/community/follow/$memberId");
+
+      if (response.statusCode == 200) {
+        return response.data["data"];
+      } else {
+        return throw Exception(
+            "API 요청이 실패했습니다. - ${response.data["error"]["code"]} ${response.data["error"]["message"]}"
+        );
+      }
+    } on Exception catch(e) {
+      return Future.error(e);
+    }
+  }
+
+  Future memberUnfollow(int memberId) async {
+    try {
+      final response = await authDio.delete("/community/follow/$memberId");
+
+      if (response.statusCode == 200) {
+        return response.data["data"];
+      } else {
+        return throw Exception(
+            "API 요청이 실패했습니다. - ${response.data["error"]["code"]} ${response.data["error"]["message"]}"
+        );
+      }
+    } on Exception catch(e) {
+      return Future.error(e);
+    }
+  }
+
+  Future getMemberProfile(
+      int memberId, int page, int size
+      ) async {
+    try {
+      final response = await authDio.get("/community/member/$memberId");
+
+      if (response.statusCode == 200) {
+        return response.data["data"];
+      } else {
+        return throw Exception(
+            "API 요청이 실패했습니다. - ${response.data["error"]["code"]} ${response.data["error"]["message"]}"
+        );
+      }
+    } on Exception catch(e) {
+      return Future.error(e);
+    }
+  }
+}
