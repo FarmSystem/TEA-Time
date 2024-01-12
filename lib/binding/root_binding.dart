105d3fa5ee9fdd1c08410e10b8082ed71ef941a2
kurtyoon <akakertcobain@naver.com>
2024-01-12 16:01:56 +0900

    [Feat] Root 페이지에 대한 바인딩을 추가한다.

--- lib/binding/root_binding.dart
+++ lib/binding/root_binding.dart
@@ -1,13 +1,11 @@
 import 'package:get/get.dart';
-import 'package:tea_time/viewModel/Entry/login_view_model.dart';
-import 'package:tea_time/viewModel/Root/root_view_model.dart';
-import 'package:tea_time/viewModel/join/sign_up_view_model.dart';
+import 'package:tea_time/viewModel/root/root_view_model.dart';
+import 'package:tea_time/viewModel/home/home_view_model.dart';
 
 class RootBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(RootViewModel());
-    Get.lazyPut(() => SignInViewModel());
-    Get.lazyPut(() => SignUpViewModel());
+    Get.lazyPut(() => HomeViewModel());
   }
 }
