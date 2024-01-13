6c60844c575cdf78b6730abb305d8b567a32380b
kurtyoon <akakertcobain@naver.com>
2024-01-14 07:25:22 +0900

    [Refactor] 회원 팔로우 관련 파일을 삭제한다.

--- teatime-api/src/main/java/farm/teatimeapi/service/CommunityService.java
+++ teatime-api/src/main/java/farm/teatimeapi/service/CommunityService.java
@@ -1,16 +1,13 @@
 package farm.teatimeapi.service;
 
-import farm.teatimeapi.dto.community.response.CommunityDto;
 import farm.teatimeapi.dto.community.response.FindMemberDto;
 import farm.teatimeapi.dto.community.response.GetCommunityDto;
 import farm.teatimeapi.dto.community.response.MemberProfileDto;
 import farm.teatimecore.exception.CustomException;
 import farm.teatimecore.exception.ErrorCode;
 import farm.teatimedomain.domain.Diary;
-import farm.teatimedomain.domain.Following;
 import farm.teatimedomain.domain.User;
 import farm.teatimedomain.reposiotry.DiaryRepository;
-import farm.teatimedomain.reposiotry.FollowingRepository;
 import farm.teatimedomain.reposiotry.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -25,7 +22,6 @@ import java.util.List;
 public class CommunityService {
     private final UserRepository userRepository;
     private final DiaryRepository diaryRepository;
-    private final FollowingRepository followingRepository;
     public GetCommunityDto getCommunity(int page, int size) {
         if (page < 0 || size < 0) {
             throw new CustomException(ErrorCode.INVALID_PARAMETER_ERROR);
@@ -40,37 +36,6 @@ public class CommunityService {
 
         Collections.shuffle(userList);
         return FindMemberDto.fromEntity(userList.subList(0, Math.min(5, userList.size())));
-    }
-
-    public void followMember(Long userId, Long memberId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
-        User member = userRepository.findById(memberId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
-
-        if (user.getId().equals(member.getId())) {
-            throw new CustomException(ErrorCode.USER_EQUAL_MEMBER);
-        }
-
-        if (user.getFollowings().stream().anyMatch(following -> following.getFollowing().getId().equals(member.getId()))) {
-            throw new CustomException(ErrorCode.ALREADY_FOLLOWING);
-        }
-
-        Following.of(user, member);
-    }
-
-    public void unfollowMember(Long userId, Long memberId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
-        User member = userRepository.findById(memberId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
-
-        if (user.getId().equals(member.getId())) {
-            throw new CustomException(ErrorCode.USER_EQUAL_MEMBER);
-        }
-
-        Following following = user.getFollowings().stream()
-                .filter(f -> f.getFollowing().getId().equals(member.getId()))
-                .findFirst()
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_FOLLOWING));
-
-        followingRepository.delete(following);
     }
 
     public MemberProfileDto getMemberProfile(Long memberId, int page, int size) {
