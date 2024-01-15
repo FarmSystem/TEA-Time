3c76cad29b6e5bfd82ff7d446b1187baa26d9287
kurtyoon <akakertcobain@naver.com>
2024-01-15 14:00:16 +0900

    [Feat] 다이어리 자세히보기 스크린과 관련된 Repository와 View Model을 생성한다.

---
+++ lib/model/diary/diary_detail_model.dart
@@ -0,0 +1,28 @@
+class DiaryDetailModel {
+  final int diaryId;
+  final String username;
+  final DateTime createdAt;
+  final String title;
+  final String profileImage;
+  final String diaryImage;
+
+  DiaryDetailModel({
+    required this.diaryId,
+    required this.username,
+    required this.createdAt,
+    required this.title,
+    required this.profileImage,
+    required this.diaryImage
+  });
+
+  factory DiaryDetailModel.fromJson(Map<String, dynamic> json) {
+    return DiaryDetailModel(
+      diaryId: json['diaryId'],
+      username: json['username'],
+      createdAt: DateTime.parse(json['created_at']),
+      title: json['title'],
+      profileImage: json['profile_image'],
+      diaryImage: json['diary_image'],
+    );
+  }
+}
---
+++ lib/repository/home/diary_detail_repository.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+import 'package:tea_time/model/diary/diary_detail_model.dart';
+import 'package:tea_time/provider/diary/diary_provider.dart';
+
+class DiaryDetailRepository extends GetxService {
+  late final DiaryProvider _diaryProvider;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    // Dependency Injection
+    _diaryProvider = Get.find<DiaryProvider>();
+  }
+
+  Future<DiaryDetailModel> getDiaryDetail(int id) async {
+    Map<String, dynamic> response = await _diaryProvider.getDiaryDetail(id);
+    return DiaryDetailModel.fromJson(response);
+  }
+}
