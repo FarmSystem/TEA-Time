89fa05dad14cf578ec49be8fc22f4f72e1458ee0
kurtyoon <akakertcobain@naver.com>
2024-01-17 14:03:47 +0900

    [Fix] deploy.sh 에서 올바른 platform으로 수정한다.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-docker buildx build --platform linux/amd64/v8 --load --tag kurtyoon/teatime-server:0.0.1 ./teatime-server
-docker buildx build --platform linux/amd64/v8 --load --tag kurtyoon/teatime-gateway:0.0.1 ./teatime-gateway
-docker buildx build --platform linux/amd64/v8 --load --tag kurtyoon/teatime-api:0.0.1 ./teatime-api
-docker buildx build --platform linux/amd64/v8 --load --tag kurtyoon/teatime-external:0.0.1 ./teatime-external
-docker buildx build --platform linux/amd64/v8 --load --tag kurtyoon/teatime-model:0.0.1 ./teatime-model
+docker buildx build --platform linux/amd64 --load --tag kurtyoon/teatime-server:0.0.1 ./teatime-server
+docker buildx build --platform linux/amd64 --load --tag kurtyoon/teatime-gateway:0.0.1 ./teatime-gateway
+docker buildx build --platform linux/amd64 --load --tag kurtyoon/teatime-api:0.0.1 ./teatime-api
+docker buildx build --platform linux/amd64 --load --tag kurtyoon/teatime-external:0.0.1 ./teatime-external
+docker buildx build --platform linux/amd64 --load --tag kurtyoon/teatime-model:0.0.1 ./teatime-model
 
 docker push kurtyoon/teatime-server:0.0.1
 docker push kurtyoon/teatime-gateway:0.0.1
