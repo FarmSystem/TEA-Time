d1fbcfde8e2edf70ed1de834acb1ee142f14f1b0
kurtyoon <akakertcobain@naver.com>
2024-01-21 18:22:57 +0900

    [Feat] 차트에 대한 데이터를 연동한다.

--- lib/provider/analysis/analysis_provider.dart
+++ lib/provider/analysis/analysis_provider.dart
@@ -58,17 +58,23 @@ class AnalysisProvider {
   Future<List<dynamic>> readEmotionAnalysisData(int period) async {
     List<dynamic> data = [];
 
-    for (int i = 0; i < period; i++) {
-      data.add({
-        "score": Random().nextDouble() * 50.0 + Random().nextDouble() * 50.0,
-        "happy": 0.0,
-        "anxiety": 0.0,
-        "anger": 0.0,
-        "embarrassment": 0.0,
-        "sadness": 0.0,
-      });
-    }
+    try {
+      final response = await authDio.get("/analysis/chart?period=$period");
 
-    return data;
+      if (response.statusCode == 200) {
+        for (int i = 0; i < period; i++) {
+          data.add({
+            "score": response.data["data"][i]["score"],
+          });
+        }
+        return data;
+      } else {
+        return throw Exception(
+          "API 요청이 실패했습니다. - ${response.data["error"]["code"]} ${response.data["error"]["message"]}"
+        );
+      }
+    } on Exception catch(e) {
+      return Future.error(e);
+    }
   }
 }
